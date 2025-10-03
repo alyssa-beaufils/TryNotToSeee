71758c10c1fdc3c7e5f17304e8669a44670e255c
thomasbrh <thomas.bruch@student.heaj.be>
2025-10-03 11:32:15 +0200

    index rework

---
+++ src/scripts/phaser.js
@@ -0,0 +1,34 @@
+// 'use strict'; // Attention de bien mettre type="module" dans la balise script du html
+
+// import Phaser from "phaser";
+
+// const config = {
+//     type: Phaser.AUTO,
+//     width: /* 800, */ window.innerWidth, // pour avoir full screen
+//     height: /* 600, */  window.innerHeight, //pour avoir full screen
+//     scene: {
+//         preload,
+//         create,
+//         update,
+//     },
+//     /* physics: {
+//         default: 'arcade',
+//         arcade: { gravity : { y : 0 } }
+//     } */
+// };
+
+// new Phaser.Game(config);
+
+// function preload(){
+
+// }
+
+// function create(){
+
+// }
+
+// function update(){
+
+// }
+
+// console.log('Phaser');
